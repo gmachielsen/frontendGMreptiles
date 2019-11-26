8ca291324b33de5b853aaeafc9dd78a50c21bf6d
Gijs Machielsen <g.machielsen@gmail.com>
2019-11-26 10:04:50 +0100

    init

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/signin" exact component={Signin} />
+                    <Route path="/signup" exact component={Signup} />
+                </div>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
