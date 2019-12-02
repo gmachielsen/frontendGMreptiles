b370e4c4904537d6fc365aed58fde07b4d82dfd5
Gijs Machielsen <g.machielsen@gmail.com>
2019-12-02 21:39:16 +0100

    initial commit before presentation

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect } from "react"
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth"
 import { Link } from "react-router-dom";
-import { listOrders } from "./apiAdmin";
+import { listOrders, getStatusValues, updateOrderStatus } from "./apiAdmin";
 import moment from "moment";
 
 const Orders = () => {
@@ -21,9 +21,20 @@ const Orders = () => {
         });
     };
 
+    const loadStatusValues = () => {
+        getStatusValues(user._id, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setStatusValues(data);
+            }
+        });
+    };
+
   useEffect(() => {
-    loadOrders()
-  }, [])
+    loadOrders();
+    loadStatusValues();
+  }, []);
 
   const showOrdersLength = () => {
     if(orders.length > 0) {
@@ -34,6 +45,40 @@ const Orders = () => {
       return <h1 className="text-danger">No orders</h1>
     }
   };
+
+  const showInput = (key, value) => (
+    <div className="input-group mb-2 mr-sm-2">
+        <div className="input-group-prepend">
+            <div className="input-group-text">{key}</div>
+        </div>
+        <input type="text" value={value} className="form-control" readOnly />
+    </div>
+  );
+
+  const handleStatusChange = (e, orderId) => {
+    updateOrderStatus(user._id, token, orderId, e.target.value).then(data => {
+      if(data.error) {
+        console.log("Status update failed")
+      } else {
+        loadOrders();
+      }
+    })
+  }
+
+  const showStatus = o => (
+    <div className="form-group">
+      <h3 className="mark mb-4">Status: {o.status}</h3>
+      <select className="form-control" onChange={(e) => handleStatusChange(e, o._id)}>
+        <option>Update Status</option>
+        {statusValues.map((status, index) => (
+          <option key={index} value={status}>
+              {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+
   return (
       <Layout
           title="Orders"
@@ -49,7 +94,7 @@ const Orders = () => {
                           <span className="bg-primary">Order Id: {o._id}</span>
                         </h2>
                         <ul className="list-group mb-2">
-                            <li className="list-group-item">{o.status}</li>
+                            <li className="list-group-item">{showStatus(o)}</li>
                             <li className="list-group-item">Transaction Id: {o.transaction_id}</li>
                             <li className="list-group-item">Order Amount ${o.user.name}</li>
                             <li className="list-group-item">Ordered on: {moment(o.createdAt).fromNow()}</li>
@@ -58,7 +103,17 @@ const Orders = () => {
 
                         </ul>
                         <h3 className="mt-4 mb-4 font-italic">
-                        Total products in the order {o.products.length}</h3>
+                        Total products in the order {o.products.length}
+                        </h3>
+                        {o.products.map((p, pIndex) => (
+                            <div className="mb-4" key={pIndex} style={{padding: "20px", border:"1px solid indigo"}}>
+                                  {showInput("Product name", p.name)}
+                                  {showInput("Product price", p.price)}
+                                  {showInput("Product total", p.count)}
+                                  {showInput("Product Id", p._id)}
+
+                            </div>
+                        ))}
                     </div>
                   )
                 })}
